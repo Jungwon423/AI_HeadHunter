bcd66382a51b0d823517b16dcfc4b9ab5e77b1cd
oreo <ohsimon77@naver.com>
2023-08-03 20:16:39 +0900

    travel Container

--- src/pages/travel.tsx
+++ src/pages/travel.tsx
@@ -1,3 +1,4 @@
+import TravelContainer from '../travel_components/TravelContainer'
 import TravelMap from '../travel_components/TravelMap'
 import Guide from '../travel_components/guide'
 
@@ -5,6 +6,7 @@ const TravelCoursePage = () => {
   return (
     <div className="flex h-screen">
       <Guide></Guide>
+      <TravelContainer></TravelContainer>
       <TravelMap></TravelMap>
     </div>
   )
