84166becf059ca554c4040ce3b20f90ea3ba19fe
oreo <ohsimon77@naver.com>
2023-08-07 17:48:22 +0900

    carousel test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,52 @@ import ImageExplain from '../index_components/ImageExplain'
 import { Footer } from '../footer/Footer'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment, selectValue } from '../slices/counterSlice'
+import Slider from 'react-slick'
+import Responsive from '../index_components/ex'
+import Slick from '../index_components/Slick'
 
+interface itemsProps {
+  item: string
+  name: string
+}
+
+const items: itemsProps[] = [
+  {
+    item: 'http://placehold.it/1200x400',
+    name: '이미지01',
+  },
+  {
+    item: 'http://placehold.it/1200x400/ff0000',
+    name: '이미지02',
+  },
+  {
+    item: 'http://placehold.it/1200x400/00ffff',
+    name: '이미지03',
+  },
+]
+
+function Item() {
+  return (
+    <Slick>
+      {items.map((item, index) => (
+        <div key={index}>
+          <img src={item.item} alt={item.name} />
+        </div>
+      ))}
+    </Slick>
+  )
+}
 export default function Home() {
   const TOKEN =
     'pk.eyJ1IjoiemlnZGVhbCIsImEiOiJjbGtrcGNwdXQwNm1oM2xvZTJ5Z2Q4djk5In0._rw_aFaBfUjQC-tjkV53Aw'
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
 
   return (
     <div className="bg-white min-h-screen">
@@ -30,6 +72,40 @@ export default function Home() {
         <MainTitle></MainTitle>
 
         <MapboxGeocoderContainer accessToken={TOKEN} />
+        <div className="container mx-auto">
+          <h1 className="text-4xl mb-8">Auto Carousel Demo</h1>
+        </div>
+        <Responsive></Responsive>
+        <Slick>
+          {items.map((item, index) => (
+            <div key={index}>
+              <img src={item.item} alt={item.name} />
+            </div>
+          ))}
+        </Slick>
+        <div>
+          <h2> Single Item</h2>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
+        </div>
 
         <TitleImage></TitleImage>
         <ImageExplain
