22d544e9725eb4c8b6a5cf860da3c4ec6b0bb3ba
Jungwon423 <44633262+Jungwon423@users.noreply.github.com>
2023-08-09 19:43:15 +0900

    /preference 수정

--- src/pages/recommend.tsx
+++ src/pages/recommend.tsx
@@ -49,7 +49,7 @@ const RecommendPage = () => {
           />
           <div className="flex flex-col md:w-1/2 md:pl-4">
             <h2 className="text-2xl font-bold mb-2">{place.name}</h2>
-            <p className="text-gray-600 text-lg mb-2"></p>
+            <p className="text-gray-600 text-lg mb-2">
               {place.summary?.overview}
             </p>
             <ul className="list-disc pl-4 mb-2">
