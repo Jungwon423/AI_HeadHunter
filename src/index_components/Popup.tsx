11eaf74b32f9a1a8592aee77d629d6762626b51d
oreo <ohsimon77@naver.com>
2023-08-16 17:05:52 +0900

    button 생성

--- src/index_components/Popup.tsx
+++ src/index_components/Popup.tsx
@@ -10,6 +10,7 @@ import {
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch } from '../store'
 import { selectCity } from '../slices/travelInfoSlice'
+import ButtonWithImage from './ButtonWithImage'
 
 interface PopupProps {
   isOpen: boolean
@@ -18,7 +19,6 @@ interface PopupProps {
 
 const Popup: React.FC<PopupProps> = ({ isOpen, onClose }) => {
   // const city = useSelector(selectCity)
-  if (!isOpen) return null
   const dispatch = useDispatch<AppDispatch>()
   useEffect(() => {
     const cityInput: CityInput = {
@@ -29,7 +29,30 @@ const Popup: React.FC<PopupProps> = ({ isOpen, onClose }) => {
   const cityInfos = useSelector(selectCityDetail)
   console.log(cityInfos.city_detail)
   const cityDetail = cityInfos.city_detail
-
+  let shortestFlightDuration = cityDetail?.shortestFlightInfo?.duration
+  const flightText =
+    Math.floor(shortestFlightDuration! / 60) +
+    '시간 ' +
+    (shortestFlightDuration! % 60) +
+    '분'
+  let splittedVisa = cityDetail?.visaInfo.description.split(',')
+  let timeDifference = cityDetail?.timezone.offset! - 540
+  let timeDifferenceText = ''
+  if (timeDifference == 0) {
+    timeDifferenceText = '한국과 같음'
+  } else if (timeDifference > 0) {
+    timeDifferenceText =
+      '한국보다 ' + Math.floor(timeDifference! / 60) + '시간 빠름'
+  } else {
+    timeDifferenceText =
+      '한국보다 ' + Math.floor(timeDifference! / 60) + '시간 느림'
+  }
+  console.log(cityDetail?.countryInfo.plug[0])
+  const frequency = cityDetail?.countryInfo.plug[0].frequency.replace(
+    /(\s*)/g,
+    '',
+  ) //공백제거
+  if (!isOpen) return null
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-20">
       {/* {children} */}
@@ -40,7 +63,7 @@ const Popup: React.FC<PopupProps> = ({ isOpen, onClose }) => {
         >
           X
         </button>
-        <div className="flex-col w-96 mx-6">
+        <div className="flex-col w-80 mx-6">
           <div className="text-2xl font-bold">{cityDetail?.name_ko}</div>
           <div className="mb-5 text-xl text-gray-700 font-medium">
             {cityDetail?.name_en}
@@ -48,72 +71,122 @@ const Popup: React.FC<PopupProps> = ({ isOpen, onClose }) => {
           <div className="text-xs text-gray-500 font-bold">
             {cityDetail?.descriptionInfo.legacy}
           </div>
-          <div className="flex">
-            <button className="flex flex-col px-2 mr-2 py-2 bg-gray-50 rounded-md shadow-sm">
-              <div className="flex flex-row pb-3">
-                <Image
-                  src="/assets/buttonIcon/calendar.webp"
-                  alt="추천시기"
-                  width={25}
-                  height={25}
-                  style={{
-                    height: 'auto',
-                  }}
-                  className="px-1"
-                ></Image>
-                <div className="pl-1 text-[8px]">추천 시기</div>
-              </div>
-              {cityDetail?.weatherRecommend.season.map((month, index) => (
-                <div className="pl-1 text-[8px]" key={index}>
-                  {month}
-                </div>
-              ))}
-            </button>
-            <button className="flex flex-col px-2 py-2 bg-gray-50 rounded-md shadow-sm">
-              <div className="flex flex-row pb-3">
-                <Image
-                  src="/assets/buttonIcon/plane.png"
-                  alt="항공"
-                  width={25}
-                  height={25}
-                  style={{
-                    height: 'auto',
-                  }}
-                  className="px-1"
-                ></Image>
-                <div className="pl-1 text-[8px]">항공</div>
-              </div>
-              <div className="pl-1 text-[8px]">
-                {cityDetail?.shortestFlightInfo!.duration}
-              </div>
-            </button>
-
-            <button className="flex flex-col px-2 py-2 bg-gray-50 rounded-md shadow-sm">
-              <div className="flex flex-row pb-3">
-                <Image
-                  src="/assets/buttonIcon/boarding-pass.png"
-                  alt="비자"
-                  width={25}
-                  height={25}
-                  style={{
-                    height: 'auto',
-                  }}
-                  className="px-1"
-                ></Image>
-                <div className="pl-1 text-[8px]">비자</div>
-              </div>
-              <div className="pl-1 text-[8px]">
-                {cityDetail?.visaInfo.description}
-              </div>
-            </button>
+          <div className="flex pt-1">
+            {cityDetail?.weatherRecommend.season ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/calendar.webp"
+                text="추천"
+                detailText={
+                  <div>
+                    {cityDetail?.weatherRecommend.season.map((month, index) => (
+                      <div className="pl-1 text-[8px]" key={index}>
+                        {month}
+                      </div>
+                    ))}
+                  </div>
+                }
+              />
+            ) : null}
+            {flightText ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/plane.png"
+                text="항공"
+                detailText={<div className="pl-1 text-[8px]">{flightText}</div>}
+              />
+            ) : null}
+            {splittedVisa ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/boarding-pass.png"
+                text="비자"
+                detailText={
+                  <div>
+                    <div className="text-[7px] text-gray-600 font-bold">
+                      {splittedVisa![0]}
+                    </div>
+                    <div className="pl-1 text-[8px]">{splittedVisa![1]}</div>
+                  </div>
+                }
+              />
+            ) : null}
+            {cityDetail?.countryInfo.currencyInformation.exchangeRate ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/exchange.png"
+                text="환율"
+                detailText={
+                  <div className="pl-1 text-[8px]">
+                    {cityDetail?.countryInfo.currencyInformation.exchangeRate}
+                  </div>
+                }
+              />
+            ) : null}
           </div>
 
-          <button className="relative top-5 right-1 text-sm px-4 py-2 bg-gray-500 text-white rounded-md">
-            더보기
-          </button>
-          <button className="relative top-5 right-1 text-sm px-4 py-2 bg-indigo-500 text-white rounded-md">
-            일정 만들기 {'>'}
-          </button>
+          <div className="flex flex-row">
+            {timeDifferenceText ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/time-zones.png"
+                text="시차"
+                detailText={
+                  <div className="text-[7px]">{timeDifferenceText}</div>
+                }
+              />
+            ) : null}
+            {cityDetail?.priceInfo.shortDescription ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/price-tag.png"
+                text="물가"
+                detailText={
+                  <div>
+                    <div className="text-[5px] text-gray-600 font-bold">
+                      한국대비
+                    </div>
+                    <div className="text-[8px]">
+                      {cityDetail?.priceInfo.shortDescription}
+                    </div>
+                  </div>
+                }
+              />
+            ) : null}
+            {cityDetail?.countryInfo.plug[0] ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/plug-in.png"
+                text="전압"
+                detailText={
+                  <div>
+                    <div className="text-[6px] text-gray-600 font-bold">
+                      {frequency}
+                    </div>
+                    <div className="text-[8px]">
+                      {cityDetail?.countryInfo.plug[0].electricPotential}
+                    </div>
+                  </div>
+                }
+              />
+            ) : null}
+            {cityDetail?.language ? (
+              <ButtonWithImage
+                imageSrc="/assets/buttonIcon/languages.png"
+                text="언어"
+                detailText={
+                  <div>
+                    {cityDetail?.language.langList.map((lang, index) => (
+                      <div className="pl-1 text-[8px]" key={index}>
+                        {lang}
+                      </div>
+                    ))}
+                  </div>
+                }
+              />
+            ) : null}
+          </div>
+          <div className="pt-1 flex justify-between">
+            <button className="flex text-sm px-6 py-2 bg-gray-500 text-white rounded-md">
+              도시 정보 더 보기
+            </button>
+            <button className="flex text-sm px-8 py-2 bg-indigo-500 text-white rounded-md">
+              일정 만들기 {'>'}
+            </button>
+          </div>
         </div>
         <div className="flex bg-white">
           <Image
@@ -121,8 +194,8 @@ const Popup: React.FC<PopupProps> = ({ isOpen, onClose }) => {
             className="rounded"
             src={cityDetail?.image.photoURL!}
             alt="travel"
-            width={200}
-            height={250}
+            width={350}
+            height={350}
             quality={100}
             style={{
               maxWidth: '100%',
